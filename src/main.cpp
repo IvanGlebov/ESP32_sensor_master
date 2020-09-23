8bfab170fd5b85ef2acecc0ae87360f40e3c0231
Ivan Glebov <glebov.vanya8@gmail.com>
2020-09-23 18:11:04 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+// MASTER
+#include <Arduino.h>
+#include <Wire.h>
+
+void setup() {
+  Wire.begin();        // join i2c bus (address optional for master)
+  Serial.begin(115200);  // start serial for output
+}
+
+void loop() {
+  Wire.requestFrom(8, 28);    // request 6 bytes from slave device #8
+  
+  while (Wire.available() > 0) { // slave may send less than requested
+    char c = Wire.read(); // receive a byte as character
+    Serial.print(c);         // print the character
+  }
+  Serial.println();
+  //Serial.println("Attempt to read data");
+  delay(1000);
+}

a2e18e356d92e5f019cba765af5bf146ee51a7dd
Ivan Glebov <glebov.vanya8@gmail.com>
2020-10-15 23:11:36 +0300

    Strange error with "Brownout detector was triggered"

--- src/main.cpp
+++ src/main.cpp
@@ -2,75 +2,39 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <EEPROM.h>
+#include "PCF8574.h"
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #define BLYNK_PRINT Serial
 
-char auth[] = "YourAuthToken";
-char ssid[] = "YourNetworkName";
-char pass[] = "YourPassword";
+char auth[] = "Rz8hI-YjZVfUY7qQb8hJGBFh48SuUn84";
+char ssid[] = "Keenetic-4926";
+char pass[] = "Q4WmFQTa";
 
-
-
-#define relay1 2
-#define relay2 4
-#define relay3 12
-#define relay4 5
-#define relay5 16 
-#define relay6 17
-#define relay7 18
-#define relay8 19
-#define relay9 14
-#define relay10 15
-#define relay11 23
-#define relay12 25
-#define relay13 26
-#define relay14 27
-#define relay15 32
-#define relay16 33
-
+PCF8574 pcf_1(0x20);
+PCF8574 pcf_2(0x21);
 
 #define slavesNumber 1
 #define debug true
 
 enum modes {automatic, manual, timeControlled, alert};
-
+BlynkTimer relaysUsing;
+bool setFlag = false;
 /* virtual pins mapping
 V0 - mode
 V10 - relay1 control 
 ^
 |
-V26 - relay16 control
+V25 - relay16 control
 
 
 
 */
 
-// Функиця для получения режима от Blynk
-BLYNK_WRITE(V1)
-{
-  int a = param.asInt();
-  // Если всё в штатном режиме, то меняем режим по указу Blynk
-  if (obj1.getMode() != alert)
-    obj1.changeModeTo(a);
-};
-
-// Реле 1. Можно управлять с Blynk только в режиме manual
-BLYNK_WRITE(V10)
-{
-  int a = param.asInt();
-  if (obj1.getMode() == manual)
-    obj1.pump04_1.setState( (a == 0)? true : false);
-}
 
 
 
-
-
-
-// Прототип функции 
-void switchRelayTo(int relayNumber, bool state);
 
 // Structure for packet variables saving
 struct packetData{
@@ -115,19 +79,27 @@ void dropBorders(borderValues &b1){
 // Класс описывающий одно конкретное реле
 class relay{
   private:
-    int pin;
+    
     String name;
     bool state;
   public:
-    relay(int bindpin, String relayName) : pin(bindpin), name(relayName) {};
-    relay(int bindPin, String relayName, bool defaultState) : pin(bindPin), name(relayName), state(defaultState) {};
-    void setBindpin(int bindPin){ pin = bindPin; }
+    int number;
+    relay(int setNumber, String relayName) {number = setNumber; name = relayName;};
+    relay(int setNumber, String relayName, bool defaultState) { number = setNumber; name = relayName; state = defaultState; };
+    void setBindpin(int setNumber){ number = setNumber; }
     void setState(bool newState){ state  = newState; }
-    void printInfo(){ Serial.println("Relay with name " + name + " on pin " + String(pin) + " is " + (state)? String(true) : String(false)); }
+    void printInfo(){ Serial.println("Relay with name " + name + " on pin " + String(number) + " is " + (state)? String(true) : String(false)); }
     void on(){ state = true; }
     void off(){ state = false; }
+    bool returnState(){ return state; }
 };
 
+void changeRelays(){}
+// Функция для применения значений реле
+void setRelay(relay r1);
+void callRelays();
+void setFlagTrue();
+// void switchRelayTo(int relayNumber, bool state);
 
 class workObj{
   private:
@@ -137,23 +109,20 @@ class workObj{
     borderValues borders;
   public:
 
-
-    // Пины реле со старта объявленыы в начале кода
-    // Реле для каждого исполнителя
-    relay pump04_1 = relay(relay1, "Pump0.4KVt-1");
-    relay pump04_2 = relay(relay2, "Pump0.4KVt-2");
-    relay pump04_3 = relay(relay3, "Pump0.4KVt-3");
-    relay valve1 = relay(relay4, "Valve-1");
-    relay valve2 = relay(relay5, "Valve-2");
-    relay light3 = relay(relay6, "Light3KVt");
-    relay light1_1 = relay(relay7, "Light1KVt");
-    relay light1_2 = relay(relay8, "Light1KVt");
-    relay light01_1 = relay(relay9, "Light0.1KVt-1");
-    relay light01_2 = relay(relay10, "Light0.1KVt-2");
-    relay destr1_1 = relay(relay11, "Destr1KVt-1");
-    relay destr1_2 = relay(relay12, "Destr1KVt-2");
-    relay siod1 = relay(relay13, "SIOD1KVt");
-
+    relay pump04_1 = relay(1, "Pump0.4Kv-1");
+    relay pump04_2 = relay(2, "Pump0.4Kv-2");
+    relay pump04_3 = relay(3, "Pump0.4Kv-3");
+    relay valve_1 = relay(4, "Valve-1");
+    relay valve_2 = relay(5, "Valve-2");
+    relay light3_1 = relay(6, "Light3Kv-1");
+    relay light1_1 = relay(7, "Light1Kv-1");
+    relay light1_2 = relay(8, "Light1Kv-2");
+    relay light01_1 = relay(9, "Light0.1KV-1");
+    relay light01_2 = relay(10, "Light0.1Kv-2");
+    relay distrif1_1 = relay(11, "Distrificator1Kv-1");
+    relay distrif1_2 = relay(12, "Distrificator1Kv-1");
+    relay steamgen1_1 = relay(13, "SteamGenerator1Kv-1");
+    relay siod1_1 = relay(14, "SIOD1Kv");
 
 
     // If setAllDefaultFlag is false - border values will be recovered from EEPROM
@@ -232,6 +201,28 @@ class workObj{
       borders.highLightLevel = highLightLevel;
     }
     
+    
+
+
+    void useRelays() const {
+      // Relays are working in inverted mode - 0 is ON, 1 is OFF
+      setRelay(pump04_1);
+      setRelay(pump04_2);
+      setRelay(pump04_3);
+      setRelay(valve_1);
+      setRelay(valve_2);
+      setRelay(light3_1);
+      setRelay(light1_1);
+      setRelay(light1_2);
+      setRelay(light01_1);
+      setRelay(light01_2);
+      setRelay(distrif1_1);
+      setRelay(distrif1_2);
+      setRelay(steamgen1_1);
+      setRelay(siod1_1);
+    }
+
+
     // Функция для автоматического полива по границам для каждого блока сенсоров
     bool autoGroundHum(int sensorBlocknumber){
       // Watering
@@ -320,28 +311,242 @@ void showPackage(packetData);
 // Зачаток функции для опроса всех блоков сенсоров разом
 void slavesQuery();
 
-workObj obj1(0, true);
+workObj obj1(1, true);
+
+
+// Прототип функции 
+
+
+
+
+// Функиця для получения режима от Blynk
+BLYNK_WRITE(V1)
+{
+  int a = param.asInt();
+  // Если всё в штатном режиме, то меняем режим по указу Blynk
+  if (obj1.getMode() != alert)
+    obj1.changeModeTo(a);
+};
+
+// Реле 1. Можно управлять с Blynk только в режиме manual
+// Pump04_1
+BLYNK_WRITE(V10)
+{
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.pump04_1.setState( (a == 0)? true : false );
+}
+// Реле 2
+// Pump04_2
+BLYNK_WRITE(V11){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.pump04_2.setState( (a == 0)? true : false );
+}
+
+// Реле 3
+// Pump04_4
+BLYNK_WRITE(V12){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.pump04_3.setState( (a == 0)? true : false );
+}
+
+// Реле 4
+// valve1_1
+BLYNK_WRITE(V13){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.valve_1.setState( (a == 0)? true : false );
+}
+
+// Реле 5
+// valve1_2
+BLYNK_WRITE(V14){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.valve_2.setState( (a == 0)? true : false );
+}
+
+// Реле 6
+// light3_1
+BLYNK_WRITE(V15){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.light3_1.setState( (a == 0)? true : false );
+}
+
+// Реле 7
+// light1_1
+BLYNK_WRITE(V16){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.light1_1.setState( (a == 0)? true : false );
+}
+
+// Реле 8
+// light1_2
+BLYNK_WRITE(V17){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.light1_2.setState( (a == 0)? true : false );
+}
+
+// Реле 9
+// light01_1
+BLYNK_WRITE(V18){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.light01_1.setState( (a == 0)? true : false );
+}
+
+// Реле 10
+// light01_2
+BLYNK_WRITE(V19){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.light01_2.setState( (a == 0)? true : false );
+}
+
+// Реле 11
+// distrif1_1
+BLYNK_WRITE(V20){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.distrif1_1.setState( (a == 0)? true : false );
+}
+
+// Реле 12
+// distrif1_2
+BLYNK_WRITE(V21){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.distrif1_2.setState( (a == 0)? true : false );
+}
+
+// Реле 13
+// steamgen1_1
+BLYNK_WRITE(V22){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.steamgen1_1.setState( (a == 0)? true : false );
+}
+
+// Реле 14
+// siod1_1
+BLYNK_WRITE(V23){
+  int a = param.asInt();
+  if (obj1.getMode() == manual)
+    obj1.siod1_1.setState( (a == 0)? true : false );
+}
+
+// Реле 15
+// empty
+BLYNK_WRITE(V24){
+  int a = param.asInt();
+  
+}
+
+// Реле 16
+// empty
+BLYNK_WRITE(V25){
+  int a = param.asInt();
+  
+}
+
+
 
 void setup() {
-  Wire.begin();        // join i2c bus (address optional for master)
+  // Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(115200);  // start serial for output
   // obj1.initRelays(relay1, relay2, relay3);
 
-  Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
-  packetData data[slavesNumber]; 
+  Blynk.begin(auth, ssid, pass, IPAddress(192,168,0,106), 8080);
+  // packetData data[slavesNumber]; 
+  // relaysUsing.setInterval(500L, setFlagTrue);
   // sensorsQuerry.setInterval(2000L, slavesQuery(data));
 }
 
 void loop() {
-  
+  // relaysUsing.run();
   // sensorsQuerry.run();
+  // callRelays();
   Blynk.run();
 
   
   
   
-  delay(1000);
+  // delay(1000);
 }
+
+
+void setFlagTrue(){
+  setFlag = true;
+}
+
+void callRelays(){
+  if (setFlag == true){
+    obj1.useRelays();
+  }
+  setFlag = false;
+}
+// Функция для применения значения реле по его номеру
+void setRelay(relay r1){
+      switch(r1.number){
+        case 1:
+          pcf_1.write(0, !r1.returnState());
+          break;
+        case 2:
+          pcf_1.write(1, !r1.returnState());
+          break;
+        case 3:
+          pcf_1.write(2, !r1.returnState());
+          break;
+        case 4:
+          pcf_1.write(3, !r1.returnState());
+          break;
+        case 5:
+          pcf_1.write(4, !r1.returnState());
+          break;
+        case 6:
+          pcf_1.write(5, !r1.returnState());
+          break;
+        case 7:
+          pcf_1.write(6, !r1.returnState());
+          break;
+        case 8:
+          pcf_1.write(7, !r1.returnState());
+          break;
+        case 9:
+          pcf_2.write(0, !r1.returnState());
+          break;
+        case 10:
+          pcf_2.write(1, !r1.returnState());
+          break;
+        case 11:
+          pcf_2.write(2, !r1.returnState());
+          break;
+        case 12:
+          pcf_2.write(3, !r1.returnState());
+          break;
+        case 13:
+          pcf_2.write(4, !r1.returnState());
+          break;
+        case 14:
+          pcf_2.write(5, !r1.returnState());
+          break;
+        case 15:
+          pcf_2.write(6, !r1.returnState());
+          break;
+        case 16:
+          pcf_2.write(7, !r1.returnState());
+          break;
+        default:
+          Serial.print("Error no such relay");
+          break;
+      }
+}
+
 
 // Функция для опроса всех блоков сенсоров разом
 // TODO - переделать тк сложно будет наладить обмен данныим между функцией и классом
